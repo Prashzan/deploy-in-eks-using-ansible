d4e0cbeef9fc5c57ee427651761d29f4c3eab612
Prashant Bhattarai <prashantbhattarai696@gmail.com>
2026-01-19 21:30:05 -0500

    adding a variables.tf file to define variable for terraform

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable vpc_cidr_block {}
-variable private_subnet_cidr_blocks {}
-variable public_subnet_cidr_blocks {}
-
 data "aws_availability_zones" "azs" {}
 
 module "myapp-vpc" {
